ed6ef83a69c74f2b3ecbca44e8ce144a9e3bda97
Hao Sun <hao.sun@appdirect.com>
2017-04-27 11:34:14 -0400

    add readme and clean up comments

---
+++ README.md
@@ -0,0 +1,6 @@
+# Genetic Algorithm to find best combination of bundles for list of bundles
+
+```
+list1 (1, 2, 3, 4, 5, 6), list2 ((1, 2), (2, 3), (3,4))
+=> list((5), (6), (1, 2), (3, 4))
+```
--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const excelToJson = require('convert-excel-to-json');
 const Je = require('jsonify-excel');
 const filepath = './test.xlsx'; /*File Upload will replace this file on every submit*/
 
+// read data from excel
 const result = excelToJson({
     sourceFile: filepath,
     outputJSON: true,
@@ -98,26 +99,6 @@ for (var o = array_n.length - 1; o >= 0; o--) {
     }
 }
 
-//var copy_of_array_n = JSON.parse(JSON.stringify(array_n));
-//res.send(JSON.stringify(array_n));
-
-// /*Combinations Generator*/
-// var combo = require("combs");
-// var array = []; //Total Visits in Array
-// /*Get to requests*/
-// for (var yu = 1; yu <= visits.length; yu++){
-//     array.push(yu);
-// }
-// /*EO tot request*/
-//
-// var iterator = combo(array,4); //All Combinations
-// for (var item of iterator) {
-//
-// }
-
-// /*EO Combinations Generator*/
-
-/*Change Visits and Prices to Int and Float*/
 var visits_fin = [];
 for (var iter = 0; iter < visits.length; iter++) {
     var spStd = visits[iter]["Standard Price"];
@@ -129,89 +110,13 @@ for (var iter = 0; iter < visits.length; iter++) {
 }
 /*EO Change Visits and Prices to Int and Float*/
 
+
+// genetic algorithm to find best bundles
 var used_bundles = [];
 
-/*
- *
- *
- *
- *
- *
- *
- *
- *
- */
 var diff;
 var bundle_lst = [];
-// while (array_n.length !== 0) {
-//     /*Greedy Algorith*/
-//     var arr_selected = []; //put all in arr_selected with largest length
-//     var SelectedBundle = {};// based on large size and min price compared to standard one
-//     var maxLen = 0, minPrice = 0;
-//
-//
-//     SelectedBundle = array_n[0][0];
-//     for (var nurse = 0; nurse < array_n.length; nurse++) {//var nurse=0; nurse<array_n.length; nurse++
-//         for (var bundle = 0; bundle < array_n[nurse].length; bundle++) {//var bundle=0; bundle<array_n[nurse].length; bundle++
-//             //Ignore Undefined bundles or NAN prices
-//             if (array_n[nurse][bundle].Bundle) {
-//
-//                 bundle_lst.push(array_n[nurse][bundle].Bundle);
-//
-//                 diff = array_n[nurse][bundle].Price;
-//                 var price_tot_for_bundle=0;
-//                 for (var i=0; i<array_n[nurse][bundle].Bundle.length; i++){
-//                     for (var x=0; x<visits_fin.length; x++){
-//                         if (visits_fin[x].requestId === array_n[nurse][bundle].Bundle[i]){
-//                             price_tot_for_bundle = price_tot_for_bundle + visits_fin[x].standardPrice;
-//                         }
-//                     }
-//                 }
-//                 array_n[nurse][bundle]["original_price"]=price_tot_for_bundle;
-//                 array_n[nurse][bundle]["diff"]=price_tot_for_bundle - array_n[nurse][bundle].Price;
-//                 if(array_n[nurse][bundle].diff > diff){
-//                     diff = price_tot_for_bundle - array_n[nurse][bundle].Price;
-//                     SelectedBundle = array_n[nurse][bundle];
-//                 }
-//
-//                 /*EO check price with standard*/
-//             }
-//         }
-//     }
-//     console.log('LIST2', JSON.stringify(bundle_lst));
-//
-//     used_bundles.push(SelectedBundle);
-//     // console.log (console.log(array_n));
-//     /*2nd Iteration - For Removing*/
-//
-//     for (var selected_bundle_route = SelectedBundle.Bundle.length - 1; selected_bundle_route >= 0; selected_bundle_route--) { //var selected_bundle_route=0;selected_bundle_route<SelectedBundle.Bundle.length; selected_bundle_route++
-//         for (var nurse_rem = array_n.length - 1; nurse_rem >= 0; nurse_rem--) {//var nurse_rem=0; nurse_rem<array_n.length; nurse_rem++
-//             for (var bundle_rem = array_n[nurse_rem].length - 1; bundle_rem >= 0; bundle_rem--) { //var bundle_rem=0; bundle_rem<array_n[nurse_rem].length; bundle_rem++
-//
-//                 if (array_n[nurse_rem][bundle_rem].Bundle) {
-//
-//                     for (var i = array_n[nurse_rem][bundle_rem].Bundle.length - 1; i >= 0; i--) { //var i = array_n[nurse_rem][bundle_rem].Bundle.length - 1; i >= 0; i--
-//                         if (array_n[nurse_rem][bundle_rem].Bundle[i] === SelectedBundle.Bundle[selected_bundle_route]) { ///***
-//                             // console.log("Match found: ", array_n[nurse_rem][bundle_rem].Bundle[i], "Delete Bundle", JSON.stringify(array_n[nurse_rem][bundle_rem]), "Indexes:", nurse_rem, "-", bundle_rem);
-//                             //Elemenate the whole bundle
-//                             array_n[nurse_rem].splice(bundle_rem, 1);
-//                             if (array_n[nurse_rem].length === 0) {
-//                                 array_n.splice(nurse_rem, 1);
-//                             }
-//                             break;
-//                         }
-//                     }
-//                 } //if bundle exists
-//             }
-//         }
-//     }
-//     // console.log("Final Array", JSON.stringify(array_n), JSON.stringify(used_bundles));
-//     arr_selected = [];
-//     SelectedBundle = {};
-//
-//     /*EO 2nd Iteration*/
-//
-// }//While
+
 
 for (var nurse = 0; nurse < array_n.length; nurse++) {//var nurse=0; nurse<array_n.length; nurse++
     for (var bundle = 0; bundle < array_n[nurse].length; bundle++) {//var bundle=0; bundle<array_n[nurse].length; bundle++
@@ -231,18 +136,16 @@ function runGenetic(cb) {
     var list1 = Array.from(Array(15)).map((e,i)=>i+1);
     var list1Cpy = JSON.parse(JSON.stringify(list1));
     var list2Cpy = JSON.parse(JSON.stringify(bundle_lst));
-// console.log('LIST1 ', JSON.stringify(list1), 'LIST2 ', JSON.stringify(bundle_lst));
     var finalResult = [];
     while (list2Cpy.length !== 0) {
         finalResult.push(genetic(list1Cpy, list2Cpy));
-        // console.log('list1cpy ', JSON.stringify(list1Cpy), 'list2Cpy ',i, JSON.stringify(list2Cpy));
     }
     console.log('LIST1 ', JSON.stringify(list1Cpy), 'LIST2 ', JSON.stringify(list2Cpy), 'FINAL ',JSON.stringify(finalResult));
     var magic = {
         left: list1Cpy,
         bundled: finalResult
     };
-    // resolve(magic);
+    // use >= if using weighted nodes
     if(finalmagic.left.length > list1Cpy) {
         finalmagic = magic
     }
